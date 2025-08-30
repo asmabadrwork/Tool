4b82eccd243d1672c8bba58bd5c1b8a04e2f59d6
Asma Badr Khan <asma.badr.work@gmail.com>
2025-08-31 01:09:18 +0530

    Adding

---
+++ sonar/terraform/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "sonarqube" {
+  ami           = "ami-0f58b397bc5c1f2e8" # Ubuntu 22.04 LTS in ap-south-1
+  instance_type = "t3.medium"
+  key_name      = "sonarqube-key"
+
+  tags = {
+    Name = "SonarQube-Server"
+  }
+}
